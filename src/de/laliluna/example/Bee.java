f4cccbf89e331678b13113cd2e56688d34cab517
Denys Triasunov <dendden@users.noreply.github.com>
2016-08-18 17:28:28 +0300

    JPA annotations for classes Honey and Bee

--- src/de/laliluna/example/Bee.java
+++ src/de/laliluna/example/Bee.java
@@ -2,11 +2,25 @@ package de.laliluna.example;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
 @SuppressWarnings("serial")
+@Entity
+@SequenceGenerator(name="bee_gen", sequenceName="bee_id_seq")
 public class Bee implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="bee_gen")
 	private Integer id;
 	private String name;
+	@ManyToOne
+	@JoinColumn //marks foreign key
 	private Honey honey;
 	
 	public Bee() {}
--- src/de/laliluna/example/Honey.java
+++ src/de/laliluna/example/Honey.java
@@ -4,12 +4,24 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
 @SuppressWarnings("serial")
+@Entity
+@SequenceGenerator(name="honey_gen", sequenceName="honey_id_seq")
 public class Honey implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="honey_gen")
 	private Integer id;
 	private String name;
 	private String taste;
+	@OneToMany(mappedBy="honey")
 	private Set<Bee> bees = new HashSet<Bee>();
 	
 	public Honey() {}
